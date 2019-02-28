6d6d49f3d84f45b6eff94dcfbd6995c1a10d49bb
ferry <ferrynuryaman01@gmail.com>
2019-02-28 16:27:52 +0700

    update

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://node-api:node-api@node-rest-shop-shard-00-00-7bbwl.mongodb.net:27017,node-rest-shop-shard-00-01-7bbwl.mongodb.net:27017,node-rest-shop-shard-00-02-7bbwl.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true', 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/nodeAPI', 
 {useNewUrlParser: true})
 
 mongoose.Promise = global.Promise;
 
 module.exports = {mongoose};
+
+//mongodb://node-api:node-api@node-rest-shop-shard-00-00-7bbwl.mongodb.net:27017,node-rest-shop-shard-00-01-7bbwl.mongodb.net:27017,node-rest-shop-shard-00-02-7bbwl.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin&retryWrites=true
